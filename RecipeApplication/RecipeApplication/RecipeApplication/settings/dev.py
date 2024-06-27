dec5211328bb8f7f4b340d0692082c642cf6dd1d
tristan_retali <tristan.retali@gmail.com>
2024-06-27 10:54:41 +0200

    Initialisation du projet

---
+++ RecipeApplication/RecipeApplication/RecipeApplication/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-zv)t&4-3++w_2r3f0o=1-oshsy#qa^u#^yp%sm7o=!$#@mox5b"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
