c883cd22abf4ea5bcc7e051c8651573e367368b0
@ajessondanie <aurelien.jesson-daniel@etu.u-bordeaux.fr>
2024-07-01 14:15:49 +0200

    ajout model

--- RecipeApplication/RecipeApplication/recipe/models.py
+++ RecipeApplication/RecipeApplication/recipe/models.py
@@ -20,17 +20,24 @@ class Recipe(models.Model):
     vegan = models.BooleanField(default=False)
     cheap = models.BooleanField(default=False)
     healthScore = models.IntegerField(default=0)
-    title = models.CharField(max_length=255)  # Utilisation du champ title
+    title = models.CharField(max_length=255)  # Utilisation du champ title  # Utilisation du champ title
     readyInMinutes = models.IntegerField(null=True, blank=True)
     servings = models.IntegerField(null=True, blank=True)
     image = models.TextField(blank=True, null=True)
     ingredients = models.ManyToManyField(Ingredient)
+    ingredients = models.ManyToManyField(Ingredient)
     summary = models.TextField(blank=True, null=True)
     cuisines = StreamField([
+        ('cuisine', CharBlock(required=True)),
         ('cuisine', CharBlock(required=True)),
     ], blank=True, null=True)
     instructions = models.TextField(blank=True, null=True)
     analyzedInstructions = StreamField([
+        ('instruction', StructBlock([
+            ('name', CharBlock(required=True)),
+            ('steps', ListBlock(StructBlock([
+                ('number', IntegerBlock(required=True)),
+                ('step', TextBlock(required=True)),
         ('instruction', StructBlock([
             ('name', CharBlock(required=True)),
             ('steps', ListBlock(StructBlock([
