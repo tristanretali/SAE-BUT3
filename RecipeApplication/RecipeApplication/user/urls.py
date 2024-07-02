1c15a408033bbb288d736da601c127b50614e9a9
tristan_retali <tristan.retali@gmail.com>
2024-07-02 10:40:52 +0200

    Ajout du logout

--- RecipeApplication/RecipeApplication/user/urls.py
+++ RecipeApplication/RecipeApplication/user/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import login, register, UserViewSet
+from .views import login, register, logout
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
-
+    path('logout/', logout, name='logout'),
 ]
--- RecipeApplication/RecipeApplication/user/views.py
+++ RecipeApplication/RecipeApplication/user/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login as django_login, logout as django_logout
 from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 
@@ -35,15 +35,35 @@ def login(request):
             json_data = json.loads(request.body)
             user = User.objects.get(username=json_data['username'], password=json_data['password'])
             if user is not None:
+                django_login(request, user)
+                request.session['user_id'] = user.id
                 return JsonResponse({
-                    'username': user.username,
-                    'email': user.email,
-                })
+                        'user_id': user.id,
+                        'username': user.username,
+                        'email': user.email,
+                    })
             else:
                 return JsonResponse({"detail": "Invalid credentials"}, status=401)
 
         except json.JSONDecodeError:
             return JsonResponse({"detail": "Invalid JSON"}, status=400)
+    else:
+        return HttpResponseBadRequest("Only POST method is supported")
+
+
+@csrf_exempt
+def logout(request):
+    if request.method == 'POST':
+        try:
+            user_id = request.session.get('user_id')
+            if user_id:
+                django_logout(request)
+                return HttpResponse('Logout successful')
+            else:
+                return JsonResponse({"detail": "User is not logged in"}, status=400)
+
+        except Exception as e:
+            return HttpResponse('Error logging out: {}'.format(str(e)), status=500)
     else:
         return HttpResponseBadRequest("Only POST method is supported")
 
@@ -54,4 +74,3 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('username')
     serializer_class = UserSerializer
-    #permission_classes = [permissions.IsAuthenticated]
