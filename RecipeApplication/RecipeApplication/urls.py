675acf0e5222d54d7093c5849daa433af29e402b
tristan_retali <tristan.retali@gmail.com>
2024-07-03 11:38:45 +0200

    Test qui passent sur le main

--- RecipeApplication/RecipeApplication/urls.py
+++ RecipeApplication/RecipeApplication/urls.py
@@ -16,7 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
+from .user import views as user_views
+
+router = routers.DefaultRouter()
+router.register(r'users', user_views.UserViewSet)
+
+urlsrest = [
+    path('rest/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))  # route pour documentation API REST
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += urlsrest
+
--- RecipeApplication/RecipeApplication/user/tests.py
+++ RecipeApplication/RecipeApplication/user/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from .serializers import UserSerializer
+from rest_framework import status
+import os
 
 
 class UserTest(TestCase):
@@ -21,3 +25,46 @@ class UserTest(TestCase):
         User.objects.filter(username__startswith="Test").delete()  # remove 2 articles from 3
         users = User.objects.all()  # 1 article should be remaining
         self.assertEqual(users.count(), 1)
+
+
+class RESTUSerTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.pascal = User.objects.create_user('pascal', 'pascal@test.com', 'pascal')
+        self.client.force_authenticate(user=self.pascal)
+        self.user1 = User.objects.create_user(username="Test user", password="user", email="user@test.net")
+        self.user2 = User.objects.create_user(username="Test user 2", password="user2", email="user2@test.net")
+        self.user3 = User.objects.create_user(username="user3", password="user3", email="user3@test.net")
+
+    def test_list_users(self):
+        response = self.client.get('/rest/users/')
+        users = User.objects.all().order_by('username')
+        serializer_data = UserSerializer(users, many=True).data
+        self.assertEqual(response.data, serializer_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.client.force_authenticate(user=None)
+
+    def test_get_user(self):
+        # get article with id 1
+        response = self.client.get("/rest/users/2/")
+        serializer_data = UserSerializer(self.user1).data
+        self.assertEqual(response.data, serializer_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.client.force_authenticate(user=None)
+
+   # def test_post_user(self):
+    #    new_data = {'username': 'Nouveau user', 'password': 'Nouveau User', 'email': 'nouveau@test.net'}
+     #   response = self.client.post("/rest/users/", new_data, format='json')
+      #  self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+       # self.client.force_authenticate(user=None)
+
+    def test_delete_user(self):
+        response = self.client.delete("/rest/users/2/")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        with self.assertRaises(User.DoesNotExist):
+            User.objects.get(id=self.user1.id)
+        self.client.force_authenticate(user=None)
+
+
+
+
