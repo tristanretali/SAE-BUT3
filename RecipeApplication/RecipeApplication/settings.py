5a61a906016a079211c0203f111462521d0ea73a
tristan_retali <tristan.retali@gmail.com>
2024-07-03 09:31:00 +0200

    Premier test

--- RecipeApplication/RecipeApplication/settings.py
+++ RecipeApplication/RecipeApplication/settings.py
@@ -9,11 +9,10 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -30,8 +29,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'user.apps.UserConfig'
-    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- RecipeApplication/RecipeApplication/user/tests.py
+++ RecipeApplication/RecipeApplication/user/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class UserTest(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username="Test user", password="user", email="user@test.net")
+        user2 = User.objects.create_user(username="Test user 2", password="user2", email="user2@test.net")
+        user3 = User.objects.create_user(username="user3", password="user3", email="user3@test.net")
+
+    def test_number_user(self):
+        users = User.objects.filter(username__startswith="Test")
+        self.assertEqual(users.count(), 2)
