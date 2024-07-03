155aef068f1eb1be2273177778b78ebd67a72c99
@ajessondanie <aurelien.jesson-daniel@etu.u-bordeaux.fr>
2024-07-03 16:23:22 +0200

    ajout parametres ingredients

--- RecipeApplication/RecipeApplication/recipe/views.py
+++ RecipeApplication/RecipeApplication/recipe/views.py
@@ -47,68 +47,79 @@ class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all().order_by('title')
     serializer_class = RecipeSerializer
     @action(detail=False, methods=['get'])
-    def search(self,request):
+    def search(self, request):
         try:
             title = request.GET.get('title')
-            if title:
-                # Récupère les recettes qui contiennent 'title' dans leur nom
-                recipes = Recipe.objects.filter(title__icontains=title)
-                # enleve tout ceux qui ont '' comme nom
-                recipes = [recipe for recipe in recipes if recipe.title != '']
+            ingredients = request.GET.getlist('ingredients')  # Récupère la liste des ingrédients
 
-                # return the list of recipes
-                return JsonResponse({
-                    'recipes': [
-                        {
-                            'title': recipe.title,
-                            'readyInMinutes': recipe.readyInMinutes,
-                            'servings': recipe.servings,
-                            'image': recipe.image,
-                            'summary': recipe.summary,
-                            'instructions': recipe.instructions,
-                            'healthScore': recipe.healthScore,
-                            'vegetarian': recipe.vegetarian,
-                            'vegan': recipe.vegan,
-                            'cheap': recipe.cheap,
-                            'cuisines': [cuisine.name for cuisine in recipe.cuisines.all()],
-                            'ingredients': [
-                                {
-                                    'name': ingredient.nameClean,
-                                    'amount': ingredient.amount,
-                                    'unit': ingredient.unit,
-                                }
-                                for ingredient in recipe.ingredients.all()
-                            ],
-                            'analyzedInstructions': [
-                                {
-                                    'name': analyzedInstruction.name,
-                                    'steps': [
-                                        {
-                                            'number': step.number,
-                                            'step': step.step,
-                                            # show ingredient names, amounts and units
-                                            'ingredients': [
-                                                {
-                                                    'name': ingredient.nameClean,
-                                                    'amount': ingredient.amount,
-                                                    'unit': ingredient.unit,
-                                                }
-                                                for ingredient in step.ingredients.all()
-                                            ],
-                                            'equipment': [equipment.name for equipment in step.equipment.all()],
-                                            'length_number': step.length_number,
-                                            'length_unit': step.length_unit,
-                                        }
-                                        for step in analyzedInstruction.steps.all()
-                                    ]
-                                }
-                                for analyzedInstruction in recipe.analyzedInstructions.all()
-                            ]
-                        }
-                        for recipe in recipes
-                    ]
-                })
+            if title or ingredients:
+                # Récupère les recettes qui contiennent 'title' dans leur nom
+                recipes = Recipe.objects.all()
+                if title:
+                    recipes = recipes.filter(title__icontains=title)
+
+                if ingredients:
+                    # Filtrer les recettes pour ne garder que celles qui contiennent tous les ingrédients spécifiés
+                    for ingredient in ingredients:
+                        recipes = recipes.filter(ingredients__nameClean__icontains=ingredient)
+
+                # Enlever toutes les recettes qui ont '' comme nom
+                recipes = [recipe for recipe in recipes if recipe.title != '']
             else:
-                return JsonResponse({"detail": "Missing 'title' parameter"}, status=400)
+                # Si aucun titre ni ingrédients n'est fourni, récupérer les 10 premières recettes
+                recipes = Recipe.objects.all()[:10]
+
+            # Retourner la liste des recettes
+            return JsonResponse({
+                'recipes': [
+                    {
+                        'title': recipe.title,
+                        'readyInMinutes': recipe.readyInMinutes,
+                        'servings': recipe.servings,
+                        'image': recipe.image,
+                        'summary': recipe.summary,
+                        'instructions': recipe.instructions,
+                        'healthScore': recipe.healthScore,
+                        'vegetarian': recipe.vegetarian,
+                        'vegan': recipe.vegan,
+                        'cheap': recipe.cheap,
+                        'cuisines': [cuisine.name for cuisine in recipe.cuisines.all()],
+                        'ingredients': [
+                            {
+                                'name': ingredient.nameClean,
+                                'amount': ingredient.amount,
+                                'unit': ingredient.unit,
+                            }
+                            for ingredient in recipe.ingredients.all()
+                        ],
+                        'analyzedInstructions': [
+                            {
+                                'name': analyzedInstruction.name,
+                                'steps': [
+                                    {
+                                        'number': step.number,
+                                        'step': step.step,
+                                        # Montrer les noms des ingrédients, les quantités et les unités
+                                        'ingredients': [
+                                            {
+                                                'name': ingredient.nameClean,
+                                                'amount': ingredient.amount,
+                                                'unit': ingredient.unit,
+                                            }
+                                            for ingredient in step.ingredients.all()
+                                        ],
+                                        'equipment': [equipment.name for equipment in step.equipment.all()],
+                                        'length_number': step.length_number,
+                                        'length_unit': step.length_unit,
+                                    }
+                                    for step in analyzedInstruction.steps.all()
+                                ]
+                            }
+                            for analyzedInstruction in recipe.analyzedInstructions.all()
+                        ]
+                    }
+                    for recipe in recipes
+                ]
+            })
         except Exception as e:
             return JsonResponse({"detail": f"Error getting recipes by title: {str(e)}"}, status=500)
