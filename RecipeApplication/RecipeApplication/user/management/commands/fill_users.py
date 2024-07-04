c7daea340c10ce539da23ba19d66cc5009dcb7b9
tristan_retali <tristan.retali@gmail.com>
2024-07-04 14:25:00 +0200

    Ajout du paramètre -p pour définir le password des users créés

--- RecipeApplication/RecipeApplication/user/management/commands/fill_users.py
+++ RecipeApplication/RecipeApplication/user/management/commands/fill_users.py
@@ -7,14 +7,15 @@ class Command(BaseCommand):
     help = 'Ajoute des utilisateurs à la base de données'
 
     def add_arguments(self, parser):
-        parser.add_argument('-n', '--number', type=str, help='Number of users')
+        parser.add_argument('-n', '--number', type=int, help='Number of users')
+        parser.add_argument('-p', '--password', type=str, help='Password of the users')
 
     def handle(self, *args, **kwargs):
         fake = Faker()
 
-        for i in range(int(kwargs['number'])):
+        for i in range(kwargs['number']):
             username = fake.user_name()
             email = fake.email()
-            User.objects.create_user(username=username, email=email, password="password")
+            User.objects.create_user(username=username, email=email, password=kwargs['password'])
 
         self.stdout.write(self.style.SUCCESS('Ajout des utilisateurs terminé'))
