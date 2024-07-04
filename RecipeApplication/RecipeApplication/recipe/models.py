f359f16a8f76d9c1f2f6fa1bafef7ce6710fe6d4
@ajessondanie <aurelien.jesson-daniel@etu.u-bordeaux.fr>
2024-07-04 16:03:30 +0200

    ajout favori, remove favori, show favori

--- RecipeApplication/RecipeApplication/recipe/models.py
+++ RecipeApplication/RecipeApplication/recipe/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from wagtail.admin.panels import FieldPanel
 from wagtail.snippets.models import register_snippet
+from django.contrib.auth.models import User
 
 
 class Ingredient(models.Model):
